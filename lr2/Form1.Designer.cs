04919f08ac58ebfe52b6843079fd98194618e533
Фащук Степан Валерьевич <sayanaraboy490@gmail.com>
2025-03-22 18:39:26 +0300

    1part lr2

--- lr2/Form1.Designer.cs
+++ lr2/Form1.Designer.cs
@@ -33,7 +33,10 @@
             this.button3 = new System.Windows.Forms.Button();
             this.button4 = new System.Windows.Forms.Button();
             this.label1 = new System.Windows.Forms.Label();
-            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
             this.SuspendLayout();
             // 
             // button1
@@ -42,61 +45,97 @@
             this.button1.Name = "button1";
             this.button1.Size = new System.Drawing.Size(137, 63);
             this.button1.TabIndex = 0;
-            this.button1.Text = "Вычислить и удалить";
+            this.button1.Text = "Вычислить";
             this.button1.UseVisualStyleBackColor = true;
             this.button1.Click += new System.EventHandler(this.button1_Click);
             // 
             // button2
             // 
-            this.button2.Location = new System.Drawing.Point(459, 261);
+            this.button2.Location = new System.Drawing.Point(266, 261);
             this.button2.Name = "button2";
             this.button2.Size = new System.Drawing.Size(138, 62);
             this.button2.TabIndex = 1;
-            this.button2.Text = "Строка";
+            this.button2.Text = "++--";
             this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
             // 
             // button3
             // 
-            this.button3.Location = new System.Drawing.Point(652, 260);
+            this.button3.Location = new System.Drawing.Point(458, 259);
             this.button3.Name = "button3";
             this.button3.Size = new System.Drawing.Size(137, 63);
             this.button3.TabIndex = 2;
-            this.button3.Text = "Логика";
+            this.button3.Text = "Строка";
             this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
             // 
             // button4
             // 
-            this.button4.Location = new System.Drawing.Point(266, 259);
+            this.button4.Location = new System.Drawing.Point(653, 259);
             this.button4.Name = "button4";
             this.button4.Size = new System.Drawing.Size(137, 64);
             this.button4.TabIndex = 3;
-            this.button4.Text = "++--";
+            this.button4.Text = "Логика";
             this.button4.UseVisualStyleBackColor = true;
             this.button4.Click += new System.EventHandler(this.button4_Click);
             // 
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(356, 24);
+            this.label1.Font = new System.Drawing.Font("Impact", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label1.Location = new System.Drawing.Point(321, 19);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(138, 13);
+            this.label1.Size = new System.Drawing.Size(228, 25);
             this.label1.TabIndex = 4;
             this.label1.Text = "Лабараторная работа №2";
-            this.label1.Click += new System.EventHandler(this.label1_Click);
+            //this.label1.Click += new System.EventHandler(this.label1_Click);
             // 
-            // textBox1
+            // label2
             // 
-            this.textBox1.Location = new System.Drawing.Point(43, 207);
-            this.textBox1.Name = "textBox1";
-            this.textBox1.Size = new System.Drawing.Size(90, 20);
-            this.textBox1.TabIndex = 5;
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(70, 220);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(62, 13);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "Результат:";
+            
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(263, 220);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(62, 13);
+            this.label3.TabIndex = 6;
+            this.label3.Text = "Результат:";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(455, 220);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(62, 13);
+            this.label4.TabIndex = 7;
+            this.label4.Text = "Результат:";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(650, 220);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(62, 13);
+            this.label5.TabIndex = 8;
+            this.label5.Text = "Результат:";
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(861, 365);
-            this.Controls.Add(this.textBox1);
+            this.ClientSize = new System.Drawing.Size(875, 356);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
             this.Controls.Add(this.label1);
             this.Controls.Add(this.button4);
             this.Controls.Add(this.button3);
@@ -116,7 +155,10 @@
         private System.Windows.Forms.Button button3;
         private System.Windows.Forms.Button button4;
         private System.Windows.Forms.Label label1;
-        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
     }
 }
 
--- lr2/Form1.cs
+++ lr2/Form1.cs
@@ -19,17 +19,40 @@ namespace lr2
 
         private void button1_Click(object sender, EventArgs e)
         {
+            int number = 15;
+            number = number + 10;
+            number = 36 * 15;
+            number = 12 - (47 / 7);
+            number += 10;
+            number *= 3;
+            number = 71 / 3;
+            label2.Text = "Результат:" + number.ToString();
 
+        }
+        private void button2_Click(object sender, EventArgs e)
+        { 
+            int count = 0;
+            count++;
+            count--;
+            label3.Text = "Результат:" + count.ToString();
+        }
+        private void button3_Click(object sender, EventArgs e)
+        {
+            string result = "Hello";
+            result += " again " + result;
+            label4.Text = "Результат:" + result.ToString();
         }
 
         private void button4_Click(object sender, EventArgs e)
         {
-
+            bool yesNo = false;
+            bool anotherBool = true;
+            yesNo = !anotherBool;
+            label5.Text = "Результат:" + yesNo.ToString();
         }
 
-        private void label1_Click(object sender, EventArgs e)
-        {
+    
 
-        }
+        
     }
 }
