d139fa5bfe068a58e4fd3e37e294166924a99a3c
Фащук Степан Валерьевич <sayanaraboy490@gmail.com>
2025-03-22 17:23:27 +0300

    button now

--- lr2/Form1.cs
+++ lr2/Form1.cs
@@ -16,5 +16,20 @@ namespace lr2
         {
             InitializeComponent();
         }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
     }
 }
